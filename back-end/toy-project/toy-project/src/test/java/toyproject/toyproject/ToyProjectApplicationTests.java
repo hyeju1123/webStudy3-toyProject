e5c8a221d8e3dabdc367149bca427d08744dbf78
hyeju1123 <hjjun1123@gmail.com>
2020-11-07 22:17:13 +0900

    BE 환경설정

---
+++ back-end/toy-project/toy-project/src/test/java/toyproject/toyproject/ToyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package toyproject.toyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
