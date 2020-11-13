b5fff1270b597094e5c7cee4d06f01412ae60642
hyeju1123 <hjjun1123@gmail.com>
2020-11-13 18:47:39 +0900

    11.13 webconfig

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,28 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import { Route } from 'react-router-dom';
+import LogIn from './authentication/LogIn';
+import SignUp from './authentication/SignUp';
 import './App.css';
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch('api/hello')
-      .then(response => response.text())
-      .then(message => {
-        setMessage(message);
-      });
-  }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">{message}</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        TEST TEST TEST!!!
-      </header>
+    <div>
+      <Route path="/login" component={LogIn} />
+      <Route path="/signup" component={SignUp} />
     </div>
   );
 }
